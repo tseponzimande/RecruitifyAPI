cec807fcfb9a88f22dbfb3705c13e8e282782cd2
Tshepo <Tshepon@blackmagic.co.za>
2026-05-24 21:00:32 +0200

    Foundation & Core Setup

---
+++ Recruitify.Domain/Common/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Recruitify.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ Recruitify.Domain/Entities/Department.cs
@@ -0,0 +1,19 @@
+﻿namespace Recruitify.Domain.Entities
+{
+    public class Department :BaseEntity
+    {
+        #region Properties
+
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+
+        #endregion
+
+        #region Navigation
+
+        public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+        public ICollection<Position> Positions { get; set; } = new List<Position>();
+
+        #endregion
+    }
+}
